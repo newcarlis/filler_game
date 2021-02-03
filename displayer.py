d3c209e3c5aac129908085542e881d0a09e98ec0
newcarlis <carly.maggiolo@gmail.com>
2021-02-03 13:57:22 -0500

    init function can now draw the entire board. There's also a new function to draw individual squares.

--- displayer.py
+++ displayer.py
@@ -4,12 +4,12 @@ from matrix_struct import *
 file that takes care of displaying game board 
 """
 
-# each block in the game is expected to be 30 pixels
-BLOCK_SIZE = 30
+# each square in the game is expected to be 30 pixels
+SQR_SIZE = 30
 # padding space to compensate for space taken by window
 PADDING = 50
 # space between blocks and window margin
-SPACE = 10
+SPACE = 5
 
 def init(matrix: Matrix):
     """
@@ -18,9 +18,11 @@ def init(matrix: Matrix):
     :return: None
     """
 
+    turtle.tracer(10, 10)
+
     # size the window so all blocks can fit
-    width = (matrix.size * BLOCK_SIZE) + PADDING
-    height = (matrix.size * BLOCK_SIZE) + PADDING
+    width = (matrix.size * SQR_SIZE) + PADDING
+    height = (matrix.size * SQR_SIZE) + PADDING
 
     # define the game window
     game_window = turtle.Screen()
@@ -30,12 +32,42 @@ def init(matrix: Matrix):
     game_window.setworldcoordinates(-SPACE, -SPACE, width, height)
 
 
-# test main function
+    # loops trough the matrix board and draws each square
+    for row in range(matrix.size):
+        for col in range(matrix.size):
+            draw_sqaure(matrix.board[row][col])
+
+def draw_sqaure(sqr: Square):
+    """
+    used the information associated with the square to draw it on the board
+    :param sqr: the square to be drawn
+    :return: TODO: return type
+    """
+
+    turtle.up()
+    turtle.goto(sqr.row * SQR_SIZE, sqr.col * SQR_SIZE)
+
+    # code to draw a square
+    # make sure the turtle is pointing east
+    # turtle.setheading(0)
+
+    # set the color
+    turtle.color(sqr.content)
+    turtle.down()
+    turtle.begin_fill()
+    for i in range(4):
+        turtle.forward(30)
+        turtle.left(90)
+    turtle.end_fill()
+
+    # make sure to return to start position
+    # turtle.goto(0, 0)
+
+# test main function TODO: remove this
 def main():
-    matrix = init_matrix(4)
-    turtle.speed(0)
-    # print(matrix)
+    matrix = init_matrix(10)
     init(matrix)
+    turtle.mainloop()
     turtle.done()
 
 
