82b7360b06d300f57f7ff8b3c9fd1839be30676c
newcarlis <carly.maggiolo@gmail.com>
2021-03-29 01:50:36 -0400

    implemented some of the functionality of game class

---
+++ game/game.py
@@ -0,0 +1,31 @@
+import sys
+
+class Game:
+    
+    def __init__(size: int, player: str):
+        self.board = Board(size)
+        self._player = Player(player)
+
+
+def terminal_mode():
+    return
+
+def window_setup():
+    return
+
+def main(invalid_counter = 1):
+    mode = input("what mode would you like?\n[1]: terminal\n[2]: window\n")
+
+    if mode == "1":
+        terminal_mode()
+    if mode == "2":
+        window_mode()
+    if invalid_counter == 3:
+        print("Too many invalid attempts. Closing the program.")
+    else:
+        print("'%s' is not valid input. Try again\n" %mode)
+        invalid_counter += 1
+        main(invalid_counter)
+
+if __name__ == "__main__":
+    main()
