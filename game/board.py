68c4f5875046f2fa41b276f9e828837b27a27811
newcarlis <carly.maggiolo@gmail.com>
2021-03-31 15:42:42 -0400

    changes to board to update tiles given a color

--- game/board.py
+++ game/board.py
@@ -3,6 +3,7 @@ import numpy as np
 from itertools import chain
 from position import Position
 from color import Color
+from queue import Queue
 
 class Board:
     """
@@ -13,7 +14,7 @@ class Board:
     size: int
         the size to give the board
     board: [[Tile]]
-    2D array containing the tiles of the game
+        2D array containing the tiles of the game
     """
     def __init__(self, size: int):
         self.size = size
@@ -71,6 +72,9 @@ class Board:
                 col = 0
             else:       
                 col += 1
+        
+        #activate relevant tiles
+        self.set_actives()
 
     def get_up(self, pos) -> Tile:
         """
@@ -136,12 +140,71 @@ class Board:
         col = pos.y + 1
         return self.board[row][col]
 
-    def get_next(tile: Tile) -> tuple:
+    def get_next(self, tile: Tile) -> tuple:
         """
         gets the adjacent objects to the one given
         :param tile: object to find adjacents for
         """
-        return (self.get_down(tile.pos), self.get_right(tile.pos))
+        down = self.get_down(tile.pos)
+        left = self.get_right(tile.pos)
+        lst = []
+        if(down is not None):
+            lst.append(down)
+        if(left is not None):
+            lst.append(left)
+        return lst
+
+    def get_tile(self, pos: Position):
+        return self.board[pos.x][pos.y]
+
+    def set_actives(self) -> None:
+        """
+        when the game starts the initial tile might have
+        adjacent ones of the same color, this ensures
+        these are activated
+        """
+        tile = self.get_tile(Position(0,0))
+        tile.active = True 
+        color = tile.color
+        self.update(color)
+
+    def update(self, color: Color):
+        """
+        updates the board with the given color
+        :param color: new color to update
+        """
+        # start at the top left corner
+        start = self.get_tile(Position(0, 0))
+        start.color = color
+
+        # keep track of changes
+        changes = 0 # TODO
+
+        # set up a queue and put starting tile
+        queue = Queue(maxsize = 3)
+        queue.put(start)
+
+        #BFS
+        while not queue.empty():
+            # deque top and get adjacents
+            tile = queue.get()
+            adjacents = self.get_next(tile)
+
+            for next_tile in adjacents:
+                if not next_tile.active:
+                    # innactive tile might match the color
+                    if next_tile.color == color:
+                        #activate and add to queue
+                        print("inactive and same color")
+                        next_tile.active = True
+                        queue.put(next_tile)
+
+                else: # active case - change to match
+                    if next_tile.color != color:
+                        print("tile is active but not same color")
+                        next_tile.color = color
+                        #add to queue
+                        queue.put(next_tile)
 
     def __iter__(self) -> object:
         """
@@ -150,6 +213,18 @@ class Board:
         return chain.from_iterable(self.board)
 
     def __str__(self) -> str:
+        """
+        returns informal representation 
+        of board - non interactive
+        for testing purposes
+        """
+        builder = ""
+        for tile in self.__iter__():
+            builder += str(tile) + "\n"
+
+        return builder
+
+    def __repr__(self) -> str:
         """
         returns formal and interactive string
         representation of board
@@ -166,15 +241,20 @@ class Board:
                 counter += 1
         return str_builder
 
-    def __repr__(self) -> str:
-        """
-        returns informal representation 
-        of board - non interactive
-        for testing purposes
-        """
-        for tile in self.__iter__():
-            print(tile)
+# b = Board(4)
+# b.pop_board()
+# tile = b.get_tile(Position(0, 0))
+# print(repr(b))
+# print(tile)
+# adj = b.get_next(tile)
+# for t in adj:
+#     print(repr(t))
+# for t in adj:
+#     t.color = Color.RED
+# for t in adj:
+#     print(repr(t))
 
-b = Board(4)
-b.pop_board()
-print(repr(b))
+
+
+# # b.pop_board()
+# print(repr(b))
