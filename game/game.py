2cace4b00fda97cc13ed522c8ad64804d025a984
newcarlis <carly.maggiolo@gmail.com>
2021-03-31 16:11:19 -0400

    reformated some

--- game/game.py
+++ game/game.py
@@ -1,4 +1,9 @@
 import sys
+from color import Color
+from player import Player
+from board import Board
+from tile import Tile
+from position import Position
 
 class Game:
     """
@@ -14,7 +19,7 @@ class Game:
     size: int
         size of the game board
     """
-    def __init__(size: int, player: str):
+    def __init__(self, size: int, player: Player):
         self.board = Board(size)
         self._player = Player(player)
 
@@ -38,6 +43,22 @@ def terminal_mode():
     ----------
     # TODO
     """
+    # size = int(input('size: ')) TODO
+    game = Game(4, Player("c"))
+    print(repr(game.board))
+    game.board.pop_board()
+    print(repr(game.board))
+    game.board.update(Color.RED)
+    print(repr(game.board))
+    game.board.update(Color.GREEN)
+    print(repr(game.board))
+    # game.update(Color.RED)
+
+    # print(repr(game.board))
+
+
+
+
     return
 
 def window_mode():
@@ -51,20 +72,21 @@ def window_mode():
     return
 
 def main(invalid_counter = 1):
-    mode = input("what mode would you like?\n[1]: terminal\n[2]: window\n[x]: cancel\n").strip()
+    terminal_mode()
+    # mode = input("what mode would you like?\n[1]: terminal\n[2]: window\n[x]: cancel\n").strip()
 
-    if mode == "1":
-        terminal_mode()
-    elif mode == "2":
-        window_mode()
-    elif mode == "x":
-        return
-    elif invalid_counter == 3:
-        print("Too many invalid attempts. Closing the program.")
-    else:
-        print("'%s' is not valid input. Try again\n" %mode)
-        invalid_counter += 1
-        main(invalid_counter)
+    # if mode == "1":
+    #     terminal_mode()
+    # elif mode == "2":
+    #     window_mode()
+    # elif mode == "x":
+    #     return
+    # elif invalid_counter == 3:
+    #     print("Too many invalid attempts. Closing the program.")
+    # else:
+    #     print("'%s' is not valid input. Try again\n" %mode)
+    #     invalid_counter += 1
+    #     main(invalid_counter)
 
 if __name__ == "__main__":
     main()
