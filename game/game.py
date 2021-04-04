7f1fb9ac1ad272c5859f3f9d5c4b32fa46c248d7
newcarlis <carly.maggiolo@gmail.com>
2021-04-04 00:22:34 -0400

    implementing terminal version with keyboard events

--- game/game.py
+++ game/game.py
@@ -4,6 +4,13 @@ from player import Player
 from board import Board
 from tile import Tile
 from position import Position
+import curses
+import time
+from sty import fg, bg, ef, rs, Style, RgbBg
+from pynput import keyboard
+
+COLR_MODE = "COLR"
+INIT_MODE = "INIT"
 
 class Game:
     """
@@ -21,7 +28,7 @@ class Game:
     """
     def __init__(self, size: int, player: Player):
         self.board = Board(size)
-        self._player = Player(player)
+        self._player = player
 
     def won(self) -> bool:
         """
@@ -35,31 +42,76 @@ class Game:
         
         return True
 
+class Terminal(Game):
+    def __init__(self, mode = INIT_MODE):
+        self.mode = mode
+        self.aceept_in = True
+        
+    def finish_init(self):
+        name = input("enter your name: ")
+        size = int(input('board dimension: '))
+        player = Player(name)
+        Game.__init__(self, size, player)
+
+    def clear(self):
+        """
+        clears the entire terminal and
+        and goes back to the top
+        """
+        print("\033[2J")
+
+    def del_n_lines(self, n: int = 0):
+        """
+        deletes n number of lines from the terminal
+        :param: n, number of lines
+        """
+        print("\033[" + str(n) + "A")
+
+    def color_op(self, option = 0):
+        colores = ""
+        counter = 0
+        for tone in Color:
+            color = tone.value
+            if counter == option:
+                colores += (bg(color[0], color[1], color[2]) + " \u2573 " + bg.rs + "\t")
+                counter += 1
+            else:
+                colores += (bg(color[0], color[1], color[2]) + "    " + bg.rs + "\t")
+                counter += 1
+        
+        return colores
+
+    def init_key_input(self):
+        with keyboard.Listener(
+            on_press= (lambda key:
+                self.on_press(key)),
+            on_release=on_release
+            ) as listener:
+            listener.join()
+
+    def on_press(self, key):
+        k = format(key)
+        
+        if self.mode == INIT_MODE:
+            print("taking initial input")
+        
+
+def on_release(key):
+    
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+    
 def terminal_mode():
-    """
-    sets up the terminal game mode
-
-    attributes
-    ----------
-    # TODO
-    """
-    # size = int(input('size: ')) TODO
-    game = Game(4, Player("c"))
-    print(repr(game.board))
-    game.board.pop_board()
-    print(repr(game.board))
-    game.board.update(Color.RED)
-    print(repr(game.board))
-    game.board.update(Color.GREEN)
-    print(repr(game.board))
-    # game.update(Color.RED)
-
+    game = Terminal()
     # print(repr(game.board))
+    # print(game.color_op())
+    game.init_key_input()
+    # self.init_key_input() # enable key input
+    finish_init() # finish initializing this object
 
 
-
-
-    return
 
 def window_mode():
     """
@@ -71,22 +123,21 @@ def window_mode():
     """
     return
 
-def main(invalid_counter = 1):
-    terminal_mode()
-    # mode = input("what mode would you like?\n[1]: terminal\n[2]: window\n[x]: cancel\n").strip()
+def propmt_mode(selector = 0):
+    options = ["exit", "terminal", "window"]
+    mode = ""
+    for i in range(len(options)):
+        if i == selector:
+            mode += "\u27A4  "
+        else:
+            mode += "   "
+        mode += (options[i] + "\n")
+    print(mode)
 
-    # if mode == "1":
-    #     terminal_mode()
-    # elif mode == "2":
-    #     window_mode()
-    # elif mode == "x":
-    #     return
-    # elif invalid_counter == 3:
-    #     print("Too many invalid attempts. Closing the program.")
-    # else:
-    #     print("'%s' is not valid input. Try again\n" %mode)
-    #     invalid_counter += 1
-    #     main(invalid_counter)
+def main():
+    
+    prompt = propmt_mode()
+    input(prompt)
 
 if __name__ == "__main__":
     main()
